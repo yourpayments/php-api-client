bc1606e3bf201278d3548c4d26fc1fd93dd4968c
Alexander Viktorchik <alexander.viktorchik@ypmn.ru>
2024-07-25 15:04:40 +0300

    qst-api - comments

--- src/Examples/qstCreateIp.php
+++ src/Examples/qstCreateIp.php
@@ -14,54 +14,78 @@ use Ypmn\QstSchemaPostAddress;
 // Подключим файл, в котором заданы параметры мерчанта
 include_once 'start.php';
 
-/* Создание и отправка анкеты для подключения продавца */
+/* Создание и отправка анкеты для подключения сабмерчанта (продавца) ИП */
 
-/* Создаем и заполняем объект анкеты */
+/* Создадим объект анкеты */
 $qst = new Qst();
 
-/* ИНН продавца */
+/* Укажем ИНН добавляемого сабмерчанта */
 $qst->setInn('773200328662');
 
-/* Данные продавца */
+/* Создадим объект данных анкеты добавляемого сабмерчанта */
 $qstSchema = new QstSchema();
+
+/* Добавим в данные анкеты номер телефона сабмерчанта */
 $qstSchema->addPhone('+7 495 1234567, доб. 123');
+
+/* Добавим в данные анкеты email сабмерчанта */
 $qstSchema->addEmail('example@ypmn.com');
 
+/* Создадим и заполним объект юридического адреса сабмерчанта */
 $qstLegalAddress = (new QstSchemaLegalAddress())
-    ->setZip('123456')
-    ->setRegion('Москва')
-    ->setCity('Москва')
-    ->setStreet('ул. Охотный ряд')
-    ->setHouse('1');
+    ->setZip('123456') // индекс
+    ->setRegion('Москва') // регион
+    ->setCity('Москва') // город
+    ->setStreet('ул. Охотный ряд') // улица
+    ->setHouse('1'); // дом
+/* Укажем объект юридического адреса сабмерчанта в данных анкеты */
 $qstSchema->setLegalAddress($qstLegalAddress);
 
+/*
+ * Создадим объект фактического адреса сабмерчанта и отметим, что
+ * фактический адрес сабмерчанта соответствует юридическому
+ */
 $qstActualAddress = (new QstSchemaActualAddress())->setChecked(true);
+/* Укажем объект фактического адреса сабмерчанта в данных анкеты */
 $qstSchema->setActualAddress($qstActualAddress);
 
+/*
+ * Создадим объект почтового адреса сабмерчанта и отметим, что
+ * почтовый адрес сабмерчанта соответствует юридическому
+ */
 $qstPostAddress = (new QstSchemaPostAddress())->setChecked(true);
+/* Укажем объект почтового адреса сабмерчанта в данных анкеты */
 $qstSchema->setPostAddress($qstPostAddress);
 
+/* Создадим объект удостоверяющего документа и заполним его паспортными данными ИП */
 $qstIdentityDoc = (new QstSchemaIdentityDoc())
-    ->setSeries('1234')
-    ->setNumber('123456')
-    ->setIssueDate('2000-01-30')
-    ->setIssuedBy('МВД')
-    ->setIssuedByKP('123-456');
+    ->setSeries('1234') // номер паспорта
+    ->setNumber('123456') // серия
+    ->setIssueDate('2000-01-30') // дата выдачи
+    ->setIssuedBy('МВД') // кем выдан
+    ->setIssuedByKP('123-456'); // к/п
 
+/*
+ * Заполним дату и место рождения ИП в данных анкеты.
+ * Укажем объект с паспортными данным ИП в данных анкеты.
+ */
 $qstSchema
-    ->setBirthDate('1969-02-23')
-    ->setBirthPlace('Москва')
-    ->setIdentityDoc($qstIdentityDoc);
+    ->setBirthDate('1969-02-23') // дата рождения ИП
+    ->setBirthPlace('Москва') // место рождения ИП
+    ->setIdentityDoc($qstIdentityDoc); // объект с паспортными данным ИП
 
+/* Создадим и заполним объект с банковскими данными ИП */
 $qstBankAccount = (new QstSchemaBankAccount())
-    ->setBankBIK('044525700')
-    ->setBankCorAccount('30101810200000000700')
-    ->setBankAccount('40702810100002400756');
-
+    ->setBankBIK('044525700') // БИК
+    ->setBankCorAccount('30101810200000000700') // кор. счет
+    ->setBankAccount('40702810100002400756'); // расч. счет
+/* Добавим объект с банковскими данными сабмерчанта в данные анкеты */
 $qstSchema->addBankAccount($qstBankAccount);
 
+/* Заполним дополнительное поле #1 (при наличии) */
 $qstSchema->setAdditionalFieldByKey(1, 'Доп. поле');
 
+/* Установим объект с данными анкеты в объект анкеты */
 $qst->setSchema($qstSchema);
 
 /* Создадим HTTP-запрос к API */
