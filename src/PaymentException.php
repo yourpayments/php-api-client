c17a7ec60a719299faed2f46e47161e79f5be486
Alexey Babak <alexey.babak@ypmn.ru>
2024-09-24 11:05:14 +0300

    PaymentException update

--- src/PaymentException.php
+++ src/PaymentException.php
@@ -4,6 +4,9 @@ namespace Ypmn;
 
 class PaymentException extends \Exception
 {
+    /** @var string Подробности для логирования */
+    private string $logText;
+
     /**
      * Ошибка в формате Bootstrap
      * @return string текст ошибки
@@ -18,5 +21,23 @@ class PaymentException extends \Exception
               ' . htmlspecialchars($this->getMessage()) . '
             </div>
         ';
+    }
+
+    /**
+     * @return string
+     */
+    public function getLogText(): string
+    {
+        return $this->logText;
+    }
+
+    /**
+     * @param string $logText
+     * @return PaymentException
+     */
+    public function setLogText(string $logText): self
+    {
+        $this->logText = $logText;
+        return $this;
     }
 }
